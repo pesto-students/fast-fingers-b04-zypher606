d8946bfc4150568213d0f79a9be64a9ff844db98
Ashim Raj Konwar <ashimrajkonwar@gmail.com>
2020-11-14 02:16:37 +0530

    Fix child ref calls

--- src/app/modules/fast-fingers/components/gameplay/gameplay.component.js
+++ src/app/modules/fast-fingers/components/gameplay/gameplay.component.js
@@ -24,12 +24,13 @@ class GameplayComponent extends React.Component {
 
         this.timeup = this.timeup.bind(this);
         this.validateResult = this.validateResult.bind(this);
-        // this.playAgain = this.playAgain.bind(this);
+        this.restartGameplay = this.restartGameplay.bind(this);
         this.startGameplay = this.startGameplay.bind(this);
 
         // this.child = React.createRef();
 
         // this.child = this.child.bind(this);
+        this.timerRef = React.createRef();
     }
 
     componentDidMount() {
@@ -39,8 +40,9 @@ class GameplayComponent extends React.Component {
     startGameplay() {
         const targetStr = this.getRandomWord(1);
         this.setState({target: targetStr});
-        this.refs.child.startTimer(2);
+        this.timerRef.current.startTimer(4);
 
+        clearInterval(this.scoreCounter);
         this.scoreCounter = setInterval(() => {
             this.setState({score: this.state.score + 1});
         }, 1000);
@@ -49,7 +51,12 @@ class GameplayComponent extends React.Component {
     continueGameplay() {
         const targetStr = this.getRandomWord(1);
         this.setState({target: targetStr});
-        this.refs.child.startTimer(2);
+        this.timerRef.current.startTimer(4);
+
+        clearInterval(this.scoreCounter);
+        this.scoreCounter = setInterval(() => {
+            this.setState({score: this.state.score + 1});
+        }, 1000);
     }
 
 
@@ -80,7 +87,7 @@ class GameplayComponent extends React.Component {
     }
 
     validateResult(result) {
-        console.log(this.state.target, result)
+        // console.log(this.state.target, result)
         if (this.state.target === result) {
             this.setState({result: ''});
             this.continueGameplay();
@@ -91,20 +98,26 @@ class GameplayComponent extends React.Component {
         this.child.method(); // do stuff
     }
 
-    playAgain() {
-        // this.startGameplay();
-        // this.child.method()
-        // this.child.current.startTimer(2);
-        window.location.reload(false);
+    restartGameplay() {
+        this.setState({ gameOver: false });
+        this.startGameplay();
+
+        // window.location.reload(false);
     }
 
-    // playAgain = () => {
-    //     this.refs.child.getWrappedInstance().startTimer(2);
+    stopGame() {
+        this.setState({ gameOver: true });
+        clearInterval(this.scoreCounter);
+        this.timerRef.current.stopTimer();
+    }
+
+    // restartGameplay = () => {
+    //     this.timerRef.current.getWrappedInstance().startTimer(2);
     //     // this.child.current.startTimer(2);
     // };
 
     render() {
-        // const playAgain = () => {
+        // const restartGameplay = () => {
         //     this.startGameplay().bind(this);
         // };
         return (
@@ -125,10 +138,10 @@ class GameplayComponent extends React.Component {
                 <br></br>
 
                 {   
-                    this.state.gameOver === false && 
-                    <div className="row inprogress">
+                    // this.state.gameOver === false && 
+                    <div className={`row inprogress ${this.state.gameOver === true ? 'hidden' : ''}`}>
                         <TimerComponent
-                            ref="child" 
+                            ref={this.timerRef} 
                             // onRef={ref => (this.child = ref)}
                             timeup={this.timeup}>
                         </TimerComponent>
@@ -142,7 +155,7 @@ class GameplayComponent extends React.Component {
                             <br></br>
                             <br></br>
                             <br></br>
-                            <a className="start-game-btn"> X STOP GAME</a>
+                            <a onClick={this.stopGame.bind(this)} className="start-game-btn"> X STOP GAME</a>
                         </div>
                     </div>
                 }
@@ -160,7 +173,7 @@ class GameplayComponent extends React.Component {
                         
 
                         <div className="col-sm-12 text-center">
-                            <a onClick={this.playAgain} className="start-game-btn"> PLAY AGAIN</a>
+                            <a onClick={this.restartGameplay.bind(this)} className="start-game-btn"> PLAY AGAIN</a>
                         </div>
                     </div>
                 }
--- src/index.js
+++ src/index.js
@@ -3,20 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app/modules/app/app';
 import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { combineReducers } from 'redux';
 
-const rootReducer = combineReducers({});
-const store = createStore(rootReducer);
+
 ReactDOM.render(
-  <Provider store={store}>
-      <App />
-   </Provider>
-  , document.getElementById('root')
-)
-
-
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
